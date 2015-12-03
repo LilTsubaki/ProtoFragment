95e1f218393e045acf330f0684a0574dba1bc31d
LilTsubaki <omenforce.clem@neuf.fr>
2015-12-03 11:00:16 +0100

    Debut plateau

---
+++ Assets/Ligne.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ligne : MonoBehaviour {
+
+    public GameObject[] cases;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Plateau.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Plateau : MonoBehaviour {
+
+    public Ligne[] lignes;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public GameObject GetCase(int x, int y)
+    {
+        return lignes[y].cases[x];
+    }
+}
